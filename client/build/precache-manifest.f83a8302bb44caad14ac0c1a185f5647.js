cbc3ca8d1eae525c9fd82986d10f05505850bffd
woodtop <cwood89@mac.com>
2019-03-06 15:56:37 -0500

    deploy to heroku

---
+++ client/build/precache-manifest.f83a8302bb44caad14ac0c1a185f5647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "05841b8f815610377da7",
+    "url": "/static/js/main.05841b8f.chunk.js"
+  },
+  {
+    "revision": "88f8f1f33a7de61ab6b1",
+    "url": "/static/js/2.88f8f1f3.chunk.js"
+  },
+  {
+    "revision": "05841b8f815610377da7",
+    "url": "/static/css/main.618efe6a.chunk.css"
+  },
+  {
+    "revision": "4b597947849b750bb53683db4700e624",
+    "url": "/index.html"
+  }
+];
